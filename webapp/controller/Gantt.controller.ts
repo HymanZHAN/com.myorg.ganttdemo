973826e7fb1a8c6446e9e1a2f84312f32a13919f
Xucong Zhan <zhanxucong@gmail.com>
2023-06-06 19:01:23 +0800

    add sub items

--- webapp/controller/Gantt.controller.ts
+++ webapp/controller/Gantt.controller.ts
@@ -3,6 +3,20 @@ import JSONModel from "sap/ui/model/json/JSONModel";
 import Format from "sap/gantt/misc/Format";
 import MessageBox from "sap/m/MessageBox";
 
+interface Shape {
+	id: string;
+	title: string;
+	startTime?: Date;
+	endTime?: Date;
+}
+
+interface Line {
+	id: string;
+	shapes: Shape[];
+	text?: string;
+	children?: Line[];
+}
+
 /**
  * @namespace com.myorg.ganttdemo.controller
  */
@@ -10,50 +24,58 @@ export default class Gantt extends Controller {
 	onInit() {
 		const oData = {
 			root: {
-				children: [],
+				children: [] as Line[],
 			},
 		};
 
 		for (let i = 0; i < 10; i++) {
-			const oLine = {
-				id: "line" + i,
+			const oLine: Line = {
+				id: `line${i}`,
 				shapes: [
 					{
-						id: "s" + i + "-1",
-						title: "Shape " + (i + 1) + " - 1",
+						id: `s${i}-1`,
+						title: `Shape(${i + 1} - 1)`,
 						startTime:
-							i === 2 ? undefined : Format.abapTimestampToDate("20181101090000"),
-						endTime: i === 2 ? undefined : Format.abapTimestampToDate("20181110090000"),
+							i === 1 ? undefined : Format.abapTimestampToDate("20181101090000"),
+						endTime: i === 1 ? undefined : Format.abapTimestampToDate("20181110090000"),
 					},
 				],
 			};
+
+			oLine.children = [1, 2, 3, 4, 5].map((k) => ({
+				id: `line${i}-${k}`,
+				shapes: [
+					{
+						id: `s${i}-${k}-1`,
+						title: `Shape(${i + 1} - ${k} - 1)`,
+						startTime:
+							k === 1 ? undefined : Format.abapTimestampToDate("20181111090000"),
+						endTime: k === 1 ? undefined : Format.abapTimestampToDate("20181127090000"),
+					},
+				],
+				text: `Row ${i + 1} - ${k}`,
+			}));
 			oLine.text = this.genLabel(i, oLine);
 			oData.root.children.push(oLine);
 		}
 		const oModel = new JSONModel(oData);
 		this.getView().setModel(oModel);
 
-		setTimeout(() => {
-			oData.root.children[0].shapes[0].startTime = undefined;
-			oData.root.children[0].shapes[0].endTime = undefined;
-			oData.root.children[0].text = this.genLabel(0, oData.root.children[0]);
+		// setTimeout(() => {
+		// 	oData.root.children[0].shapes[0].startTime = undefined;
+		// 	oData.root.children[0].shapes[0].endTime = undefined;
+		// 	oData.root.children[0].text = this.genLabel(0, oData.root.children[0]);
 
-			oData.root.children[1].shapes[0].startTime =
-				Format.abapTimestampToDate("20181111090000");
-			oData.root.children[1].shapes[0].endTime = Format.abapTimestampToDate("20181127090000");
-			oData.root.children[1].text = this.genLabel(1, oData.root.children[1]);
+		// 	oData.root.children[1].shapes[0].startTime =
+		// 		Format.abapTimestampToDate("20181111090000");
+		// 	oData.root.children[1].shapes[0].endTime = Format.abapTimestampToDate("20181127090000");
+		// 	oData.root.children[1].text = this.genLabel(1, oData.root.children[1]);
 
-			this.getView().getModel().setData(oData);
-		}, 4000);
+		// 	(this.getView().getModel() as JSONModel).setData(oData);
+		// }, 4000);
 	}
 
-	private genLabel(
-		i: number,
-		oLine: {
-			id: string;
-			shapes: { id: string; title: string; startTime: Date; endTime: Date }[];
-		}
-	): any {
+	private genLabel(i: number, oLine: Line): string {
 		return `Row ${i + 1} [${this.formatDate(oLine.shapes[0].startTime)} - ${this.formatDate(
 			oLine.shapes[0].endTime
 		)}]`;
@@ -61,7 +83,7 @@ export default class Gantt extends Controller {
 
 	getRowById(sRowId: string) {
 		const oModel: JSONModel = this.getView().getModel() as JSONModel;
-		const aRows = oModel.getData().root.children;
+		const aRows = oModel.getData().root.children as Line[];
 		for (let i = 0; i < aRows.length; i++) {
 			if (aRows[i].id === sRowId) {
 				return aRows[i];
@@ -107,8 +129,10 @@ export default class Gantt extends Controller {
 	}
 
 	fnShapeResize(oEvent: any) {
-		const oShape = oEvent.getParameter("shape"),
-			aNewTimes = oEvent.getParameter("newTime");
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		const oShape = oEvent.getParameter("shape");
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		const aNewTimes = oEvent.getParameter("newTime");
 
 		oShape.setTime(aNewTimes[0]);
 		oShape.setEndTime(aNewTimes[1]);
